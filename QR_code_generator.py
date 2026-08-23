390052e2910cb563f393c58381a8bcf8f7fc4b75
Abdullah979440 <kazi972263@gmail.com>
2026-08-23 20:16:08 +0600

    for some changes on qr

---
+++ QR_code_generator.py
@@ -0,0 +1,15 @@
+import qrcode
+
+data = input("Enter text or URL: ")
+
+qr = qrcode.QRCode(
+    box_size=10,   # QR এর সাইজ 
+    border=4       # চারপাশের সাদা বর্ডার
+)
+.add_data(data)
+qr.ke(fit=True)maqr
+
+img = qr.make_image(fill_color="blue", back_color="white")
+img.save("qr_code.png")
+
+print("QR code saved as qr_code.png")
