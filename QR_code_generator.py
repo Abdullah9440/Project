733848a4d0ed77523c6b358d7703c550105661e0
Abdullah979440 <kazi972263@gmail.com>
2026-08-23 20:18:21 +0600

    some change

--- QR_code_generator.py
+++ QR_code_generator.py
@@ -6,8 +6,8 @@ qr = qrcode.QRCode(
     box_size=10,   # QR এর সাইজ 
     border=4       # চারপাশের সাদা বর্ডার
 )
-.add_data(data)
-qr.ke(fit=True)maqr
+qr.add_data(data)
+qr.ke(fit=True)
 
 img = qr.make_image(fill_color="blue", back_color="white")
 img.save("qr_code.png")
